9646a63df5ba36a0f24ee63c2bb6fa0bce50613d
Sergio Barrera <sdbeng@gmail.com>
2018-05-18 18:33:47 -0700

    create array of objects and render list

--- server.js
+++ server.js
@@ -7,10 +7,33 @@ var PORT = process.env.PORT || 3013
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
-//1st route
+// demo data
+let title = 'cool hacker coddyy app!'
+var lunches = [
+    {lunch: "chicken salad sandwich"},
+    {lunch: "spaghetti with meatballs"},
+]
+
+//1st test route
 app.get('/', function(req, res){
-    res.render('index', {title: 'cool hacker coddyy app!'})
+    res.render('index', {title})
 })
+// get route to /weekday
+app.get('/weekday', function(req, res){
+    res.render('index', lunches[0])
+})
+// get route to /weekend
+app.get('/weekend', function(req, res){
+    res.render('index', lunches[1])
+})
+// get route to /lunches
+app.get('/lunches', function(req, res){
+    res.render('index', {
+        lunches:lunches,
+        title:title
+    })
+})
+
 
 //server
 app.listen(PORT, function(){
